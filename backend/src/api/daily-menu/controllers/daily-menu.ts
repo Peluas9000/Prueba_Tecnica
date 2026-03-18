c54e1a5aea2fbcec6a516f64e9728abbf0f04f53
Peluas9000 <ayoubbenjaddi5@gmail.com>
2026-03-18 17:42:48 +0100

    REsuleto Lifecycle y dos servicios personalizado cone l get simple de recibir todos los postres

--- backend/src/api/daily-menu/controllers/daily-menu.ts
+++ backend/src/api/daily-menu/controllers/daily-menu.ts
@@ -2,6 +2,17 @@
  * daily-menu controller
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::daily-menu.daily-menu');
+export default factories.createCoreController(
+  "api::daily-menu.daily-menu",
+  (strapi) => ({
+    async find(ctx) {
+      try {
+        const total = await strapi
+          .services("api::daily-menu.daily-menu")
+          .devolverPostresSinRepetir();
+      } catch (error) {}
+    },
+  }),
+);

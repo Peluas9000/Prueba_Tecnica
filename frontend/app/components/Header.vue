fe0c0299baeb5394905e9d380bc639610e0f8f30
Peluas9000 <ayoubbenjaddi5@gmail.com>
2026-03-20 12:40:55 +0100

    Practicas index con la carpeta reutilizable de header

---
+++ frontend/app/components/Header.vue
@@ -0,0 +1,14 @@
+<script lang="js"></script>
+
+<template>
+  <div class="header">
+    <h1>The Daily Plate</h1>
+    <div class="nav">
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+      </nav>
+    </div>
+  </div>
+</template>
